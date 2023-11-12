6872effa607bf8e2d2db615f70f4e1e51a200e75
redouane <redouane.hitachi@gmail.com>
2023-11-12 10:45:40 -0800

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-
+import  {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home' 
+import SignIn from './pages/SignIn' 
+import SignUp from './pages/SignUp' 
+import Profile from './pages/Profile'
+import About from './pages/about'
 export default function App() {
 
   console.log('app')
   return (
     <div>
-      <h1 className="text-red-500">App</h1>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-out' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/about' element={<About/>}/>
+     </Routes>
+     
+     </BrowserRouter>
     </div>
   )
 }
